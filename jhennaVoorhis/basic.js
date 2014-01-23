e5e978a91d3fc42c8e8c4e23b0fc1b23148ef907
Jhenna Voorhis <jhenna.voorhis@gmail.com>
2014-01-22 22:58:38 -0800

    completed assgn 4 basic

---
+++ jhennaVoorhis/basic.js
@@ -0,0 +1,21 @@
+//Part 1
+
+var arr1 = [1, 2, 3];
+
+//Part 2
+arr1.push(4);
+
+//Part 3
+arr1.shift();
+
+//Part 4
+console.log(arr1);
+
+//Part 5
+arr1.unshift(0.5);
+
+//Part 6
+arr1.pop();
+
+//Part 7
+console.log(arr1);
