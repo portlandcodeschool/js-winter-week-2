7f07955b1c6b8c452799ce623f78c9ac9426e388
Colin Lebens <colin.lebens@gmail.com>
2014-01-23 09:47:49 -0800

    First push for 1/22/2014 Assignment

---
+++ colinLebens/basic.js
@@ -0,0 +1,32 @@
+// Colin Lebens
+// Basic Assignment
+// js-winter-week-2
+
+// Part 1
+
+var numArray = [1, 2, 3];
+
+// Part 2
+
+numArray.push(4);
+//console.log(numArray);
+
+// Part 3
+
+numArray.shift();
+
+// Part 4
+
+console.log(numArray);
+
+// Part 5
+
+numArray.unshift(0.5);
+
+// Part 6
+
+numArray.pop();
+
+// Part 7
+
+console.log(numArray); 
---
+++ colinLebens/romnum.js
@@ -0,0 +1,33 @@
+if(!module.parent) { // Put any code you need to test your functions in here:
+
+// Example:
+console.log("53 in roman numerals is: " + decimalToRoman(53));  
+
+}
+/*-----------------------------------------------------------------------------
+     
+
+     Nothing needs to be in this area
+
+
+-----------------------------------------------------------------------------*/
+function decimalToRoman (decNumber) {
+  var romNumeral = "";
+
+  // Implement here!
+
+  return romNumeral;
+}
+
+function romanToDecimal (romNumeral) {
+  var decNumber = 0;
+
+  // Implement here!
+
+  return decNumber;
+}
+
+// Don't worry about this stuff. It's here so I can more easily test your code.
+
+module.exports.decimalToRoman = decimalToRoman;
+module.exports.romanToDecimal = romanToDecimal;
