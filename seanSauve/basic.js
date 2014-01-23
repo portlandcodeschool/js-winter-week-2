21879b460c9a8195c4edc7bddb2e32082f96b817
Sean Sauve <seantsauve@gmail.com>
2014-01-23 08:02:42 -0800

    first commit basic.js

---
+++ seanSauve/basic.js
@@ -0,0 +1,27 @@
+//Sean Sauve
+//Basic Problems Week 2
+//Javascript Winter Semester
+//Portland Code School
+
+//Part 1
+var thisArray = [1,2,3];
+
+//Part 2
+thisArray.push(4);
+
+//Part 3
+thisArray.shift();
+
+//Part 4
+console.log("verifying array is: " + thisArray);
+
+//Part 5
+thisArray.unshift(.5);
+
+//Part 6
+thisArray.pop();
+
+//Part 7
+console.log("\nverifying array is: " + thisArray);
+
+
