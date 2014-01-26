8ee5e1caf69dc809ebcbed6763b647bbd2c95066
Paul Ogden <ageoldpun@gmail.com>
2014-01-26 09:14:38 -0800

    Romnum added, but not done.  Come back to it once learning about testing

---
+++ paulOgden/romnum.js
@@ -0,0 +1,37 @@
+//I'm going to come back to this assignment once we learn about testing in JS.
+//Doing this without testing for use cases sounds like a good way to get frustrated. :)
+
+
+if(!module.parent) { // Put any code you need to test your functions in here:
+
+// Example:
+console.log("53 in roman numerals is: " + decimalToRoman(53));  
+
+}
+/*-----------------------------------------------------------------------------
+     
+
+     Nothing needs to be in this area
+
+
+-----------------------------------------------------------------------------*/
+function decimalToRoman (decNumber) {
+  var romNumeral = "";
+
+  // Implement here!
+
+  return romNumeral;
+}
+
+function romanToDecimal (romNumeral) {
+  var decNumber = 0;
+
+  // Implement here!
+
+  return decNumber;
+}
+
+// Don't worry about this stuff. It's here so I can more easily test your code.
+
+module.exports.decimalToRoman = decimalToRoman;
+module.exports.romanToDecimal = romanToDecimal;
