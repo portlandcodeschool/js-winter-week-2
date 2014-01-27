ffab8bc45bc7d0f28e6a39638645dc5fd3a3c9e0
Jenna Gretsch <pdxstarburst@gmail.com>
2014-01-27 14:40:40 -0800

    homework assignment 3

---
+++ pdxstarburst/intermediate.js
@@ -0,0 +1,68 @@
+//PCS, Jenna Gretsch/pdxstarburst
+//Homework WK 2, Wed 
+//1.25.14   Intermediate
+
+//Part 1
+/*
+function stringInArray(input1, input2) {
+    for(var i=0; i<input.length; i++) {
+        if (input[i] === input2) {
+        	return true;
+    	}
+    }
+    return false;
+	
+}
+console.log(stringInArray([1,2,"cake"] ,"cake")); 
+*/
+
+//Part 2
+
+var x = ["apple", "kiwi", "pear", "banana"];
+	function copy(x) { 
+		var y = x.slice(3)
+		return y;
+
+	}
+
+console.log(copy(x));
+
+
+
+//Part 3 
+/*
+function numberEven(apple) {
+
+	var output = [];
+	for(var i= 1;  i<apple.length; i++) {
+		if(apple[i] % 2 === 0) {
+		output.push(apple[i]); 
+		
+			
+		}
+	}
+		return output;
+
+}
+console.log(numberEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])); */
+
+
+		  
+
+
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pdxstarburst/wk2_Wed_hw.js
@@ -0,0 +1,18 @@
+//PCS, Jenna Gretsch/pdxstarburst
+//Homework WK 2, Wed 
+//1.25.14
+//Part 1
+
+var number = [1, 2, 3];
+   number.push(4);
+   number.shift(0);
+   console.log(number);
+
+   number.unshift(0.5);
+   console.log(number);
+
+   number.pop(number.length -1);
+   console.log(number);
+
+
+
