45de35b6749359a5fe8055556d319ca97002b9e2
abbystevens <adeats@gmail.com>
2014-01-26 16:15:03 -0800

    adding basic.js

---
+++ Stevens/basic.js
@@ -0,0 +1,23 @@
+//Part 1 //
+
+var numbers = [1, 2, 3];
+
+//Part 2 //
+
+numbers.push(4);
+
+//Part 3 //
+
+numbers.shift(1);
+
+//Part 4 //
+console.log(numbers);
+
+//Part 5 //
+numbers.unshift(0.5);
+
+//Part 6 //
+numbers.pop(4);
+
+//Part 7 //
+console.log(numbers);
