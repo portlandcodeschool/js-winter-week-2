9617fe2acbba311c8eba1f9278e2357318e349bc
Ian Forrest <ianforrest@gmail.com>
2014-01-26 20:31:21 -0800

    basic initial commit

---
+++ ianForrest/basic.js
@@ -0,0 +1,23 @@
+// JS WINTER 2014, WEEK 2, ASSIGNMENT 4 by Ian Forrest
+// basic.js
+
+// Part 1
+var easyArray = [1, 2, 3]
+
+// Part 2
+easyArray.push(4);
+
+// Part 3
+easyArray.shift(1);
+
+// Part 4
+console.log(easyArray);
+
+// Part 5
+easyArray.unshift(0.5);
+
+// Part 6
+easyArray.pop(4);
+
+// Part 7
+console.log(easyArray);
