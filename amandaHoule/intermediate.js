8df3adb56343123940187ef753b8ad707fdbe105
amanda <ah@ahs-macbook-pro-2.local>
2014-01-27 10:07:39 -0800

    fixed

--- amandaHoule/intermediate.js
+++ amandaHoule/intermediate.js
@@ -47,13 +47,15 @@ console.log("Part 3");
 // an array containing only the even integers from the original array.
 
 var part3Arr = [23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
+var oddNumArr = []; 
 
 part3Arr.forEach(function (item, i) {
-             if (item % 2 === 0) {
-             	console.log(i);
-             }
+            if (item % 2 === 0) {
+             	oddNumArr.push(i);
+            }
             });
 
+console.log(oddNumArr);
 
 
 
