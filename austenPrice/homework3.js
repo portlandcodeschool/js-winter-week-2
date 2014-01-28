6ba7ea7ce82eb0e0db86a646f809f0d6498210a4
AustenPrice <netsua2@gmail.com>
2014-01-27 17:35:28 -0800

    changes to last hw

--- austenPrice/homework3.js
+++ austenPrice/homework3.js
@@ -41,8 +41,8 @@ function refactor (myName) {
 
     for (var i = myName.length - 1; i >= 0; i--) {
         switch (myName[i]) {
-            case 'a': case 'e': case 'i': case 'o': case 'u': case : 'y'
-            case 'A': case 'E': case 'I': case 'O': case 'U': case : 'Y'
+            case 'a': case 'e': case 'i': case 'o': case 'u': case 'y':
+            case 'A': case 'E': case 'I': case 'O': case 'U': case 'Y':
                 break;
             default:
                 output += myName[i];
