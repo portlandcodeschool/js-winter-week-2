c38d1caed808b284eda471bfceaec1d3d693fc8e
Ceenan Calzadilla <sofocused11@gmail.com>
2014-01-27 17:57:41 -0800

    Adding HW3 basic

---
+++ ceenanCalzadilla/basic.js
@@ -0,0 +1,21 @@
+//Part1
+var basicarray = ["1" , "2" , "3"];
+
+//Part2
+basicarray.push("4");
+
+//Part3
+basicarray.shift("0");
+
+//Part4
+console.log(basicarray);
+
+//Part5
+basicarray.unshift("0.5");
+console.log(basicarray);
+
+//Part6
+basicarray.pop("4");
+
+//Part7
+console.log(basicarray);
